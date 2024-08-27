0f2a85d1c147f0e84d1b9c27253d00bf7eadd245
pravinraghul <pravinraghul@gmail.com>
2024-08-27 16:48:49 +0530

    Closes #10 Fixed the data parsing issue

--- dut/protoc.py
+++ dut/protoc.py
@@ -8,17 +8,26 @@ class Protoc:
         self.clkp = clkp
         self.datap = datap
 
+    def _wait_until_last_transaction(self):
+        while self.clkp.value() == 1:
+            pass
+
     def read_byte(self):
         byte = 0
         index = 0
         new_state = 0
         old_state = 0
+
+        self._wait_until_last_transaction()
+
         while True:
             new_state = self.clkp.value()
             if new_state != old_state:
                 if new_state: # only if the clock is high
                     byte |= (self.datap.value() << index)
                     index += 1
-            if index == 8:
-                return byte
-            old_state = new_state
+                if index == 8:
+                    break
+                old_state = new_state
+
+        return byte
